f48e5bc2156efeeaffc5fb9e0946b5ad015ee9fd
obliquedskid <angelo.paris.testematte@gmail.com>
2025-02-28 00:51:17 +0100

    Update AutoAnchorModule.java

--- 1.20.1/src/main/java/net/caffeinemc/phosphor/module/modules/combat/AutoAnchorModule.java
+++ 1.20.1/src/main/java/net/caffeinemc/phosphor/module/modules/combat/AutoAnchorModule.java
@@ -1,130 +1,216 @@
-package net.caffeinemc.phosphor.module.modules.combat;
+package dev.lvstrng.argon.module.modules.combat;
 
-import net.caffeinemc.phosphor.api.event.events.PlayerTickEvent;
-import net.caffeinemc.phosphor.api.event.orbit.EventHandler;
-import net.caffeinemc.phosphor.api.util.BlockUtils;
-import net.caffeinemc.phosphor.api.util.InvUtils;
-import net.caffeinemc.phosphor.mixin.ClientPlayerInteractionManagerAccessor;
-import net.caffeinemc.phosphor.module.Module;
-import net.caffeinemc.phosphor.module.setting.settings.BooleanSetting;
-import net.caffeinemc.phosphor.module.setting.settings.NumberSetting;
+import dev.lvstrng.argon.event.events.ItemUseListener;
+import dev.lvstrng.argon.event.events.TickListener;
+import dev.lvstrng.argon.module.Category;
+import dev.lvstrng.argon.module.Module;
+import dev.lvstrng.argon.module.setting.BooleanSetting;
+import dev.lvstrng.argon.module.setting.KeybindSetting;
+import dev.lvstrng.argon.module.setting.NumberSetting;
+import dev.lvstrng.argon.utils.*;
 import net.minecraft.block.Blocks;
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.Items;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
+import net.minecraft.item.ShieldItem;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import org.lwjgl.glfw.GLFW;
 
-public class AutoAnchorModule extends Module {
-    public final NumberSetting switchDelay = new NumberSetting("Switch Delay", this, 0, 0, 20, 1);
-    public final NumberSetting placeDelay = new NumberSetting("Place Delay", this, 0, 0, 20, 1);
-    public final NumberSetting cooldown = new NumberSetting("Cooldown", this, 0, 0, 20, 1);
-    public final NumberSetting itemSwap = new NumberSetting("Item Swap", this, 1, 1, 9, 1);
-    public final BooleanSetting chargeOnly = new BooleanSetting("Charge Only", this, false);
+import java.util.HashSet;
+import java.util.Set;
 
-    public AutoAnchorModule() {
-        super("AutoAnchor", "Automatically achors", Category.COMBAT);
-    }
+//not mine
+public final class AnchorMacro extends Module implements TickListener, ItemUseListener {
+	private final BooleanSetting whileUse = new BooleanSetting(EncryptedString.of("While Use"), true).setDescription(EncryptedString.of("If it should trigger while eating/using shield"));
+	private final BooleanSetting stopOnKill = new BooleanSetting(EncryptedString.of("Stop on Kill"), false).setDescription(EncryptedString.of("Doesn't anchor if body nearby"));
+	private final BooleanSetting clickSimulation = new BooleanSetting(EncryptedString.of("Click Simulation"), false).setDescription(EncryptedString.of("Makes the CPS hud think you're legit"));
+	private final NumberSetting switchDelay = new NumberSetting(EncryptedString.of("Switch Delay"), 0, 20, 0, 1);
+	private final NumberSetting switchChance = new NumberSetting(EncryptedString.of("Switch Chance"), 0, 100, 100, 1);
+	private final NumberSetting placeChance = new NumberSetting(EncryptedString.of("Place Chance"), 0, 100, 100, 1).setDescription(EncryptedString.of("Randomization"));
+	private final NumberSetting glowstoneDelay = new NumberSetting(EncryptedString.of("Glowstone Delay"), 0, 20, 0, 1);
+	private final NumberSetting glowstoneChance = new NumberSetting(EncryptedString.of("Glowstone Chance"), 0, 100, 100, 1);
+	private final NumberSetting explodeDelay = new NumberSetting(EncryptedString.of("Explode Delay"), 0, 20, 0, 1);
+	private final NumberSetting explodeChance = new NumberSetting(EncryptedString.of("Explode Chance"), 0, 100, 100, 1);
+	private final NumberSetting explodeSlot = new NumberSetting(EncryptedString.of("Explode Slot"), 1, 9, 1, 1);
+	private final BooleanSetting onlyOwn = new BooleanSetting(EncryptedString.of("Only Own"), false);
+	private final BooleanSetting onlyCharge = new BooleanSetting(EncryptedString.of("Only Charge"), false);
 
-    private static boolean hasAnchored;
-    private static int switchClock, placeClock, cooldownClock;
+	private int switchClock = 0;
+	private int glowstoneClock = 0;
+	private int explodeClock = 0;
 
-    private void reset() {
-        hasAnchored = false;
-        switchClock = 0;
-        placeClock = 0;
-        cooldownClock = 0;
-    }
+	//hashset cuz in a hashset stuff cant repeat iirc
+	private final Set<BlockPos> ownedAnchors = new HashSet<>();
 
-    @Override
-    public void onEnable() {
-        reset();
-    }
+	public AnchorMacro() {
+		super(EncryptedString.of("Anchor Macro"),
+				EncryptedString.of("Automatically blows up respawn anchors for you"),
+				-1,
+				Category.COMBAT);
+		addSettings(whileUse, stopOnKill, clickSimulation, placeChance, switchDelay, switchChance, glowstoneDelay, glowstoneChance, explodeDelay, explodeChance, explodeSlot, onlyOwn, onlyCharge);
+	}
 
-    @EventHandler
-    private void onPlayerTick(PlayerTickEvent event) {
-        if (mc.world == null || mc.player == null || mc.currentScreen != null)
-            return;
+	@Override
+	public void onEnable() {
+		eventManager.add(TickListener.class, this);
+		eventManager.add(ItemUseListener.class, this);
+		switchClock = 0;
+		glowstoneClock = 0;
+		explodeClock = 0;
+		super.onEnable();
+	}
 
-        if (hasAnchored) {
-            if (cooldownClock < cooldown.getIValue()) {
-                cooldownClock++;
-                return;
-            }
+	@Override
+	public void onDisable() {
+		eventManager.remove(TickListener.class, this);
+		eventManager.remove(ItemUseListener.class, this);
+		super.onDisable();
+	}
 
-            reset();
-        }
+	@Override
+	public void onTick() {
+		if (mc.currentScreen != null)
+			return;
 
-        if (GLFW.glfwGetMouseButton(mc.getWindow().getHandle(), GLFW.GLFW_MOUSE_BUTTON_RIGHT) != GLFW.GLFW_PRESS) {
-            reset();
-            return;
-        }
+		if (((mc.player.getMainHandStack().getItem().getComponents().contains(DataComponentTypes.FOOD) || mc.player.getMainHandStack().getItem() instanceof ShieldItem || mc.player.getOffHandStack().getItem() instanceof ShieldItem || mc.player.getOffHandStack().getItem().getComponents().contains(DataComponentTypes.FOOD)) && GLFW.glfwGetMouseButton(mc.getWindow().getHandle(), GLFW.GLFW_MOUSE_BUTTON_RIGHT) == GLFW.GLFW_PRESS) && !whileUse.getValue())
+			return;
 
-        if (mc.player.isUsingItem())
-            return;
+		if(stopOnKill.getValue() && WorldUtils.isDeadBodyNearby())
+			return;
 
-        if (mc.crosshairTarget instanceof BlockHitResult hit) {
-            if (hit.getType() == HitResult.Type.MISS)
-                return;
+		int randomInt = MathUtils.randomInt(1, 100);
 
-            BlockPos pos = hit.getBlockPos();
+		if (KeyUtils.isKeyPressed(GLFW.GLFW_MOUSE_BUTTON_RIGHT)) {
+			if (mc.crosshairTarget instanceof BlockHitResult hit) {
+				if (BlockUtils.isBlock(hit.getBlockPos(), Blocks.RESPAWN_ANCHOR)) {
+					if (onlyOwn.getValue() && !ownedAnchors.contains(hit.getBlockPos()))
+						return;
 
-            if (BlockUtils.isBlock(Blocks.RESPAWN_ANCHOR, pos)) {
-                mc.options.useKey.setPressed(false);
+					mc.options.useKey.setPressed(false);
 
-                if (BlockUtils.isAnchorUncharged(pos)) {
-                    if (!mc.player.isHolding(Items.GLOWSTONE)) {
-                        if (switchClock < switchDelay.getIValue()) {
-                            switchClock++;
-                            return;
-                        }
+					if (BlockUtils.isAnchorNotCharged(hit.getBlockPos())) {
+						randomInt = MathUtils.randomInt(1, 100);
 
-                        InvUtils.selectItemFromHotbar(Items.GLOWSTONE);
+						if (randomInt <= placeChance.getValueInt()) {
+							if (!mc.player.getMainHandStack().isOf(Items.GLOWSTONE)) {
+								if (switchClock != switchDelay.getValueInt()) {
+									switchClock++;
+									return;
+								}
 
-                        switchClock = 0;
-                    }
+								randomInt = MathUtils.randomInt(1, 100);
 
-                    if (placeClock < placeDelay.getIValue()) {
-                        placeClock++;
-                        return;
-                    }
+								if(randomInt <= switchChance.getValueInt()) {
+									switchClock = 0;
+									InventoryUtils.selectItemFromHotbar(Items.GLOWSTONE);
+								}
+							}
 
-                    ActionResult actionResult = mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND, hit);
-                    if (actionResult.isAccepted() && actionResult.shouldSwingHand()) {
-                        mc.player.swingHand(Hand.MAIN_HAND);
-                    }
+							if (mc.player.getMainHandStack().isOf(Items.GLOWSTONE)) {
+								if (glowstoneClock != glowstoneDelay.getValueInt()) {
+									glowstoneClock++;
+									return;
+								}
 
-                    placeClock = 0;
-                }
-                if (BlockUtils.isAnchorCharged(pos) && !chargeOnly.isEnabled()) {
-                    if (mc.player.getInventory().selectedSlot != itemSwap.getIValue() - 1) {
-                        if (switchClock < switchDelay.getIValue()) {
-                            switchClock++;
-                            return;
-                        }
+								randomInt = MathUtils.randomInt(1, 100);
 
-                        mc.player.getInventory().selectedSlot = itemSwap.getIValue() - 1;
-                        ((ClientPlayerInteractionManagerAccessor) mc.interactionManager).callSyncSelectedSlot();
+								if(randomInt <= glowstoneChance.getValueInt()) {
+									glowstoneClock = 0;
 
-                        switchClock = 0;
-                    }
+									if (clickSimulation.getValue())
+										MouseSimulation.mouseClick(GLFW.GLFW_MOUSE_BUTTON_RIGHT);
 
-                    if (placeClock < placeDelay.getIValue()) {
-                        placeClock++;
-                        return;
-                    }
+									WorldUtils.placeBlock(hit, true);
+								}
+							}
+						}
+					}
 
-                    ActionResult actionResult2 = mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND, hit);
-                    if (actionResult2.isAccepted() && actionResult2.shouldSwingHand()) {
-                        mc.player.swingHand(Hand.MAIN_HAND);
-                    }
+					if (BlockUtils.isAnchorCharged(hit.getBlockPos())) {
+						int slot = explodeSlot.getValueInt() - 1;
 
-                    placeClock = 0;
-                    hasAnchored = true;
-                }
-            }
-        }
-    }
+						if (mc.player.getInventory().selectedSlot != slot) {
+							if (switchClock != switchDelay.getValueInt()) {
+								switchClock++;
+								return;
+							}
+
+							if(randomInt <= switchChance.getValueInt()) {
+								switchClock = 0;
+								mc.player.getInventory().selectedSlot = slot;
+							}
+						}
+
+						if (mc.player.getInventory().selectedSlot == slot) {
+							if (explodeClock != explodeDelay.getValueInt()) {
+								explodeClock++;
+								return;
+							}
+
+							randomInt = MathUtils.randomInt(1, 100);
+
+							if(randomInt <= explodeChance.getValueInt()) {
+								explodeClock = 0;
+
+								if (!onlyCharge.getValue()) {
+									if (clickSimulation.getValue())
+										MouseSimulation.mouseClick(GLFW.GLFW_MOUSE_BUTTON_RIGHT);
+
+									WorldUtils.placeBlock(hit, true);
+
+									ownedAnchors.remove(hit.getBlockPos());
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	public void onItemUse(ItemUseEvent event) {
+		if (mc.crosshairTarget instanceof BlockHitResult hitResult && hitResult.getType() == HitResult.Type.BLOCK) {
+			if (mc.player.getMainHandStack().getItem() == Items.RESPAWN_ANCHOR) {
+				Direction dir = hitResult.getSide();
+				BlockPos pos = hitResult.getBlockPos();
+
+				if (!mc.world.getBlockState(pos).isReplaceable()) {
+					switch (dir) {
+						case UP: {
+							ownedAnchors.add(pos.add(0, 1, 0));
+							break;
+						}
+						case DOWN: {
+							ownedAnchors.add(pos.add(0, -1, 0));
+							break;
+						}
+						case EAST: {
+							ownedAnchors.add(pos.add(1, 0, 0));
+							break;
+						}
+						case WEST: {
+							ownedAnchors.add(pos.add(-1, 0, 0));
+							break;
+						}
+						case NORTH: {
+							ownedAnchors.add(pos.add(0, 0, -1));
+							break;
+						}
+						case SOUTH: {
+							ownedAnchors.add(pos.add(0, 0, 1));
+							break;
+						}
+					}
+				} else ownedAnchors.add(pos);
+			}
+
+			BlockPos bp = hitResult.getBlockPos();
+
+			if(BlockUtils.isAnchorCharged(bp))
+				ownedAnchors.remove(bp);
+		}
+	}
 }
